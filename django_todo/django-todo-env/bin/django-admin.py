ebe3aace3f746e429197186f21e47596ddf518fa
Yuki Ogawa <cyrano@CyranonoMacBook-Pro.local>
2019-11-22 02:30:33 +0900

    create todo project

---
+++ django_todo/django-todo-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/cyrano/Documents/Github/train-hub/django_todo/django-todo-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
